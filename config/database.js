a47ceeb7b7b5e4daf862c087660161399c64f45a
No044 <phisuka0122386@gmail.com>
2025-03-26 17:13:23 +0700

    update1

--- config/database.js
+++ config/database.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose")
 
-module.exports.connect = () => {
-    mongoose.connect(process.env.MONGO_URL).then(() => {
-        console.log("ket noi thanh cong")
-    })
-}
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("MongoDB Connection Error:", err));
