afdaf028db1104791a269882ec49cb504931c375
No044 <phisuka0122386@gmail.com>
2025-03-26 17:16:55 +0700

    update2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.error("MongoDB Connection Error:", err));
