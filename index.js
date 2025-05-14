d3077451bf1d1ac3fdf31f8c71e1099373d566e5
minhnghia2k3 <88654111+minhnghia2k3@users.noreply.github.com>
2025-05-15 00:15:33 +0700

    rebuild cicd

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const cors = require("cors")
-const database = require("./config/database")
-const routerV1 = require("./API/V1/routers/Main_Y.Router")
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const database = require("./config/database");
+const routerV1 = require("./API/V1/routers/Main_Y.Router");
 const cookieParser = require("cookie-parser");
 
-dotenv.config()
-database.connect()
+dotenv.config();
+database.connect();
 
-const app = express()
-const port = process.env.PORT
-const frontendUrl = process.env.FRONTEND_URL
-
+const app = express();
+const port = process.env.PORT;
+const frontendUrl = process.env.FRONTEND_URL;
+console.log("frontendUrl", frontendUrl);
 
 app.use(express.json());
 const allowedOrigins = [frontendUrl];
-app.use(cors({
+app.use(
+  cors({
     origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
     },
-    credentials: true 
-}));
+    credentials: true,
+  })
+);
 app.use(cookieParser());
-routerV1(app)
+routerV1(app);
 
-app.listen(port,() => {
-    console.log("listening on port ", port)
-})
+app.listen(port, () => {
+  console.log("listening on port ", port);
+});
